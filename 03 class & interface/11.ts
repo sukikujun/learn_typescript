a5db3e4e633ce100b9172d831ab5fb35eb0c674f
jyun <kikusujun@gmail.com>
2024-02-11 16:44:01 +0900

    class & interface

---
+++ 03 class & interface/11.ts
@@ -0,0 +1,36 @@
+/// 
+{
+    type Point = {
+        x: number,
+        y: number
+    }
+
+    interface AnimationInterface {
+        name: string
+        move(): void
+    }
+    abstract class Animation {
+        protected getPosition(): Point {
+            return { x: 100, y: 300 }
+        }
+    }
+
+    class Thank extends Animation implements AnimationInterface {
+        name: string = 'enemy'
+        public move(): void {
+            console.log(`${this.name} tank move`);
+        }
+    }
+
+    class Player extends Animation implements AnimationInterface {
+        name: string = 'player'
+        public move(): void {
+            console.log(`${this.name} tank move`);
+        }
+    }
+
+    const t = new Thank();
+    const p = new Player();
+    t.move()
+    p.move()
+}
