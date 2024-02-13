1ded108ea5500457949b21a05921bd9e27781cc1
sukikujun <sukikujun@gmail.com>
2024-02-13 23:47:30 +0900

    import export

---
+++ 07 webpack/src/User.ts
@@ -0,0 +1,5 @@
+export let channel: string = 'nibiiro yurushi@channel'
+
+export namespace Vtuber {
+    export let company: string = 'SONY——VEE Office'
+}
--- 07 webpack/src/index.ts
+++ 07 webpack/src/index.ts
@@ -1,1 +1,8 @@
+import { channel, Vtuber } from "./User";
+
+console.log('channel', channel);
+
+console.log(Vtuber.company);
+
+
 alert('初めまして、nibiiro yurushi　と申します！')
