858e59858e3ffd53e2c4b06019e7b9d3bcf3d0aa
sukikujun <sukikujun@gmail.com>
2024-02-13 21:00:48 +0800

    namespace

---
+++ 06 namespace/1.js
@@ -0,0 +1,12 @@
+"use strict";
+/// namespace
+var Sahiro;
+(function (Sahiro) {
+    Sahiro.u = 'sahiro';
+})(Sahiro || (Sahiro = {}));
+var Yurushi;
+(function (Yurushi) {
+    Yurushi.y = 'yurushi';
+})(Yurushi || (Yurushi = {}));
+console.log(Sahiro.u);
+console.log(Yurushi.y);
---
+++ 06 namespace/1.ts
@@ -0,0 +1,11 @@
+/// namespace
+namespace Sahiro {
+    export let u = 'sahiro'
+}
+
+namespace Yurushi {
+    export let y = 'yurushi'
+}
+
+console.log(Sahiro.u);
+console.log(Yurushi.y);
