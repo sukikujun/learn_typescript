c56cbc6e5a5114bc4bc3d4f1f511315c2ead8cd8
sukikujun <sukikujun@gmail.com>
2024-02-13 21:46:55 +0800

    tsc

---
+++ 06 namespace/6/App.ts
@@ -0,0 +1,3 @@
+import { User, title } from './User.js'
+
+document.body.insertAdjacentHTML('beforeend', `<h2>${User.name}</h2>`)
---
+++ 06 namespace/6/User.ts
@@ -0,0 +1,5 @@
+export namespace User {
+    export let name = 'yurushi'
+}
+
+export let title = 'YouTube'
