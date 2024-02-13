99af4fa0f3bce97850241626cf3d43694d8df45d
sukikujun <sukikujun@gmail.com>
2024-02-14 00:56:48 +0900

    有限集合 无限集合

---
+++ 08 type/11.js
@@ -0,0 +1,9 @@
+"use strict";
+/// 有限集合 无限集合
+{
+    let a = 'a';
+    a = 97;
+    a = false;
+    let b = 'yurushi';
+    // b = 'YURUSHI'
+}
---
+++ 08 type/11.ts
@@ -0,0 +1,12 @@
+/// 有限集合 无限集合
+{
+    type A = string | number | boolean
+    let a: A = 'a'
+    a = 97
+    a = false
+
+    type B = 'yurushi'
+    let b: B = 'yurushi'
+    // b = 'YURUSHI'
+
+}
