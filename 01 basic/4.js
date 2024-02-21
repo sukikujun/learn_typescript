e101d0e9e69e097309de7a4614204e52fe07e8c9
jyun <kikusujun@gmail.com>
2024-02-21 20:23:52 +0900

    tsc 4.ts

---
+++ 01 basic/4.js
@@ -0,0 +1,4 @@
+"use strict";
+let hd = 'houdunren';
+//型 'number' を型 'string' に割り当てることはできません。
+// hd = 2024;
---
+++ 01 basic/4.ts
@@ -0,0 +1,3 @@
+let hd = 'houdunren';
+//型 'number' を型 'string' に割り当てることはできません。
+// hd = 2024;
