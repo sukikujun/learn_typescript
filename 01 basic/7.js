0365727e4e341e69e8151fcff091a8db31322f0d
jyun <kikusujun@gmail.com>
2024-02-21 20:58:17 +0900

    类型推断

---
+++ 01 basic/7.js
@@ -0,0 +1,13 @@
+"use strict";
+{
+    let ys = 'yurushi';
+    ys = 'nibiiro yurushi';
+    let sy = 100;
+    // sy = true;
+    let flag = true;
+    flag = 1 > 2;
+    function sum(a, b) {
+        return 'result:' + a + b;
+    }
+    console.log(sum(3, 7));
+}
---
+++ 01 basic/7.ts
@@ -0,0 +1,15 @@
+{
+    let ys = 'yurushi';
+    ys = 'nibiiro yurushi';
+    let sy = 100;
+    // sy = true;
+
+    let flag:boolean = true;
+    flag = 1 > 2;
+
+    function sum(a:number, b:number): string {
+        return 'result:' + a  + b;
+    }
+
+    console.log(sum(3, 7));
+}
