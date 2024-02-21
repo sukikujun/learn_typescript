48d4ab70f12df05e5bf3abe2a65bd62b72b7e15d
jyun <kikusujun@gmail.com>
2024-02-21 21:18:19 +0900

    组合类型

---
+++ 01 basic/13.js
@@ -0,0 +1,11 @@
+"use strict";
+{
+    let ys;
+    ys = '20';
+    ys = 20;
+    console.log(ys);
+    let arr;
+    arr = ['true', true];
+    arr.push('false', false);
+    console.log();
+}
---
+++ 01 basic/13.ts
@@ -0,0 +1,14 @@
+{
+    let ys: string | number;
+    ys = '20'
+    ys = 20
+
+    console.log(ys);
+
+    let arr: Array<string | boolean>;
+    arr = ['true', true]
+    arr.push('false', false)
+
+    console.log();
+    
+}
