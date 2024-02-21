07d929d60685d4ee5dacf1922b01c73baa398e85
jyun <kikusujun@gmail.com>
2024-02-21 21:10:38 +0900

    基本数据类型

---
+++ 01 basic/11.js
@@ -0,0 +1,12 @@
+"use strict";
+{
+    let ys;
+    ys = 'nibiiro yurushi';
+    console.log(ys);
+    // 型 'string' を型 'number' に割り当てることはできません。ts(2322)
+    // let age: number = '20';
+    let arr;
+    let arr2 = [];
+    arr = [];
+    arr.push('linux', 'docker', 'php');
+}
---
+++ 01 basic/11.ts
@@ -0,0 +1,14 @@
+{
+    let ys: string;
+    ys = 'nibiiro yurushi';
+    console.log(ys);
+    
+    // 型 'string' を型 'number' に割り当てることはできません。ts(2322)
+    // let age: number = '20';
+
+    let arr: string[];
+    let arr2: Array<string> = [];
+    arr = [];
+    arr.push('linux', 'docker', 'php');
+
+}
