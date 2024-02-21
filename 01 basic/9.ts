2fab2abe2a44b72f0a7436f23cf29fec0df5ab69
jyun <kikusujun@gmail.com>
2024-02-21 21:04:44 +0900

    对象类型推断

---
+++ 01 basic/9.ts
@@ -0,0 +1,18 @@
+{
+    const user = {
+        name: 'yurushi',
+        age: 23,
+        open: true,
+        lesson: [
+            {title: 'linux'},
+            {title: 'TypeScript'},
+        ]
+    }
+    // 型 'string' を型 'number' に割り当てることはできません。ts(2322)
+    // user.age = '30';
+
+    user.lesson.push({title: 'Docker'})
+    
+    console.log(user.lesson[0].title);
+    
+}
